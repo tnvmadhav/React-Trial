67159ea3da574b4de8bbd45283d8bb46f724ab8a
T .VenuMadhav <tnvmadhav@gmail.com>
2020-12-05 21:24:13 +0530

    Message: Initialized the application codebase
    Description:
    * Practice with states and updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+
+class PasswordStrengthChecker extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {color : 'black', status: 'Enter Password', password: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.assertStrength(event.target.value);
+  }
+
+  assertStrength(password){
+    let len = password ? password.length : 0
+    return len < 1 ? this.setState({ color: 'black', status: 'Enter Password', password: password})  :
+           len < 6  ? this.setState({ color: 'red', status: 'Weak', password: password}) :
+           len < 10 ? this.setState({ color: 'orange', status: 'Good', password: password}) :
+           this.setState({ color: 'green', status: 'Strong', password: password}); 
+  }
+
+
+  render() {
+    return (
+      <div class='main-setting'>
+      <h2 class='sub-setting'> Password Strength Checker </h2>
+      <p class='sub-setting' style={{ color: this.state.color }}> {this.state.status} </p>
+      <input type="password" onChange={this.handleChange}></input><small style={{ color: 'grey' }}> {this.state.password.length}</small>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <PasswordStrengthChecker/>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  );
